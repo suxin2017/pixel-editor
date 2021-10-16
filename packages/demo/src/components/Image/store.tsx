56d1e3be85f29986199296c40bdfe4a22139fa20
suxin2017 <1107178482@qq.com>
2021-10-16 10:42:56 +0800

    update code
    M  packages/demo/src/components/Image/store.tsx

--- packages/demo/src/components/Image/store.tsx
+++ packages/demo/src/components/Image/store.tsx
@@ -1,9 +1,15 @@
+import { action, makeObservable } from "mobx";
 import { ComponentStore } from "store";
 
 export interface IImageStoreProps {
   src: string | undefined;
+  width: string;
+  height: string;
 }
 
 export class ImageStore extends ComponentStore<IImageStoreProps> {
-
+  constructor(initProps: { initProps: IImageStoreProps; name: string }) {
+    super(initProps);
+    makeObservable(this);
+  }
 }
